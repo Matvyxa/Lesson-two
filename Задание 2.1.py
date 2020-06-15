9f8e039f611f6e935e28e2c3b6c269c2d690156f
Матвей <66694005+Matvyxa@users.noreply.github.com>
2020-06-15 21:25:09 +0300

    Задание 2.1

---
+++ Задание 2.1.py
@@ -0,0 +1,7 @@
+my_list = ["Имя", 34, 4.2, True, None, [1,2]]
+print(type(my_list[0]))
+print(type(my_list[1]))
+print(type(my_list[2]))
+print(type(my_list[3]))
+print(type(my_list[4]))
+print(type(my_list[5]))
